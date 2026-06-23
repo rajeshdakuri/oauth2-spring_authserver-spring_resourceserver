f4c766b88b1bbf73a8e219b8928ddaa378b8b2e3
Rajesh <133854531+rajeshdakuri@users.noreply.github.com>
2026-06-23 19:54:48 +0530

    resource server added

---
+++ oauth2resourceserver/oauth2resourceserver/src/main/java/com/example/resourceserver/ResourceserverApplication.java
@@ -0,0 +1,12 @@
+package com.example.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceserverApplication.class, args);
+	}
+}
